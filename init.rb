75f32536afadd55df7d5fb14406899e2c5c73dc2
John Griffiths <indiehead@gmail.com>
2009-04-08 23:01:34 +0100

    fix

--- init.rb
+++ init.rb
@@ -2,3 +2,4 @@
 ActionView::Base.send(:include, HatchetHarry)
 ActiveRecord::Base.send(:include, HatchetHarry)
 ActionController::Base.send(:include, HatchetHarry)
+ActionController::Caching::Sweeper.send(:include, HatchetHarry)
