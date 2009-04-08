7e961710909dc663dbf60a4c5c16047dafce20ca
John Griffiths <indiehead@gmail.com>
2009-04-08 22:06:40 +0100

    hook

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
 # Include hook code here
-ActionView::Base.send :include, HatchetHarry
-ActionController::Base.send :include, HatchetHarry
+ActionView::Base.send(:include, HatchetHarry)
+ActiveRecord::Base.send(:include, HatchetHarry)
+ActionController::Base.send(:include, HatchetHarry)
