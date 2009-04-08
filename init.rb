00920c99eef1b0a1dbc655c5063dcb3eeb46d3a9
John Griffiths <indiehead@gmail.com>
2009-04-08 23:07:30 +0100

    fix

--- init.rb
+++ init.rb
@@ -1,4 +1,6 @@
 # Include hook code here
-ActionView::Base.send(:include, HatchetHarry)
-ActiveRecord::Base.send(:include, HatchetHarry)
-ActionController::Base.send(:include, HatchetHarry)
+#ActionView::Base.send(:include, HatchetHarry)
+#ActiveRecord::Base.send(:include, HatchetHarry)
+#ActionController::Base.send(:include, HatchetHarry)
+
+require File.dirname(__FILE__) + '/lib/hatchet_harry'
